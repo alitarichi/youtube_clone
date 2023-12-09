3a246eaf9d4342dbfeeccf0d8fcf47ddabbb4995
Ali Tarichi <tarichiali@gmail.com>
2023-12-09 20:39:57 +0300

    2nd commit

--- youtube_clone/src/App.tsx
+++ youtube_clone/src/App.tsx
@@ -1,8 +1,10 @@
 import { PageHeader } from "./layouts/PageHeader";
 
 export default function App() {
-  return <div className="">
+  return (
+   <div className="max-h-screen flex flex-col">
     <PageHeader />
     <div>2</div>
   </div>
+  )
 }
--- youtube_clone/src/layouts/PageHeader.tsx
+++ youtube_clone/src/layouts/PageHeader.tsx
@@ -1,3 +1,9 @@
 export function PageHeader() {
-    return null
+    return <div className="flex gap-10 lg:gap-20 justify-between">
+        <div className="flex gap-4 items-center flex-shrink-0">
+
+        </div>
+        <div></div>
+        <div></div>
+    </div>
 }
